cb0a87d83c5bfca775c6d1a99017dc2dcd873e32
lailaN123 <lolonj2000@gmail.com>
2023-09-07 18:58:32 -0700

    6-size.c file

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,10 +12,10 @@ int Int;
 long int Lint;
 float Float;
 long long int Llint;
-printf("Size of a char: %zu byte(s)\n", sizeof(Char)));
-printf("Size of an int: %zu byte(s)\n", sizeof(Int)));
-printf("Size of a long int: %zu byte(s)\n", sizeof(Lint)));
-printf("Size of a long long int: %zu byte(s)\n", sizeof(Llint)));
-printf("Size of a float: %zu byte(s)\n", sizeof(Float)));
+printf("Size of a char: %zu byte(s)\n", sizeof(Char));
+printf("Size of an int: %zu byte(s)\n", sizeof(Int));
+printf("Size of a long int: %zu byte(s)\n", sizeof(Lint));
+printf("Size of a long long int: %zu byte(s)\n", sizeof(Llint));
+printf("Size of a float: %zu byte(s)\n", sizeof(Float));
 return (0);
 }
