ccbae45e32b13ce23af16a32e32899ed8c131e07
lailaN123 <lolonj2000@gmail.com>
2023-09-07 18:51:49 -0700

    6-size.c file

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char Char;
+int Int;
+long int Lint;
+float Float;
+long long int Llint;
+printf("Size of a char: %zu byte(s)\n", sizeof(Char)));
+printf("Size of an int: %zu byte(s)\n", sizeof(Int)));
+printf("Size of a long int: %zu byte(s)\n", sizeof(Lint)));
+printf("Size of a long long int: %zu byte(s)\n", sizeof(Llint)));
+printf("Size of a float: %zu byte(s)\n", sizeof(Float)));
+return (0);
+}
