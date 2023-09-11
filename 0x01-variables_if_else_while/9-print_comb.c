61bd00776f374c436cea8a1a31d4365361b86257
lailaN123 <lolonj2000@gmail.com>
2023-09-11 00:36:12 -0700

    9-print_comb.c file

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 /**
- * main - prints all single digit numbers of base 10 starting from 0.
+ * main - prints all possible combinations of single-digit numbers.
  *
  * Return: Always 0 (Success)
  */
