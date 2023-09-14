5782dd40c2e6094206476b8cd215474a86b305b6
lailaN123 <lolonj2000@gmail.com>
2023-09-14 02:03:45 -0700

    104-fibonacci.c file

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include<stdio.h>
 /**
  * main -  prints the first 98 Fibonacci numbers, starting with 1 and 2.
  *
@@ -6,25 +6,23 @@
  */
 int main(void)
 {
-    unsigned long fib1, fib2, U;
-    int i;
+	unsigned long fib1, fib2;
+	unsigned long U;
+	int i;
 
-    fib1 = 1;
-    fib2 = 2;
-
-    printf("%lu", fib1);
-
-    for (i = 2; i <= 97; i++)
-    {
-        U = fib1 + fib2;
-        fib1 = fib2;
-        fib2 = U;
-
-        printf(", %lu", U);
-    }
-
-    printf("\n");
-
-    return (0);
+fib1 = 1;
+fib2 = 2;
+printf("%lu, ", fib1);
+printf("%lu, ", fib2);
+for (i = 2; i <= 97; i++)
+{
+	U = fib1 + fib2;
+	fib1 = fib2;
+	fib2 = U;
+	if (i < 97)
+	printf("%lu, ", U);
+	else
+	printf("%lu\n", U);
 }
-
+return (0);
+}
