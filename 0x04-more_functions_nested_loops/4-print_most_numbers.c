4a18b5feaa3316e7ef9d8fc1c6a713fee41223fb
lailaN123 <lolonj2000@gmail.com>
2023-09-16 18:31:10 -0700

    4-print_most_numbers.c file

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -9,12 +9,12 @@ void print_most_numbers(void)
 {
 	int i;
 
-for (i = 48; i <= 57)
+for (i = 48; i <= 57; i++)
 {
 	if (i != 50 || i != 52)
 	{
 		_putchar(i);
 	}
 }
-putchar('\n);
+putchar('\n');
 }
