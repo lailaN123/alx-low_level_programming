a0ec5a4e95ecd46d9a0237d7c8c720df2010adb9
lailaN123 <lolonj2000@gmail.com>
2023-09-21 01:08:32 -0700

    7-puts_half.c file

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,21 @@
+#include"main.h"
+#include<stdio.h>
+/**
+ * print_array - prints n elements of an array of integers
+ * @a: array to be printed
+ * @n: number of elements of the array
+ * Return: Always 0 (success).
+ */
+void print_array(int *a, int n)
+{
+int i;
+
+	for (i = 0; i < n; i++)
+	{
+	if (i < n - 1)
+		printf("%d, ", a[i]);
+	else
+		printf("%d\n", a[i]);
+	}
+}
+
