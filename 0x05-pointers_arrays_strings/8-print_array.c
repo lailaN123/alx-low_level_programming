d26806e4188c315c619a8cb2c7048644ee3b2ada
lailaN123 <lolonj2000@gmail.com>
2023-09-21 01:42:09 -0700

    8-print_array.c file

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -8,21 +8,13 @@
  */
 void print_array(int *a, int n)
 {
-int i;
+	int i = 0;
 
 	for (i = 0; i < n; i++)
-	{
-	if (n == 1)
-	{
-		printf("%d\n", a[i]);
-	}
-	else
 	{
 		if (i < n - 1)
 			printf("%d, ", a[i]);
 		else
 			printf("%d\n", a[i]);
 	}
-	}
 }
-
