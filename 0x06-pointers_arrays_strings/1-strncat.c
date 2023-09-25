3bc6edf5a95b9c806b8c42cc3211263e900c3089
lailaN123 <lolonj2000@gmail.com>
2023-09-24 21:42:20 -0700

    1-strncat.c file

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -12,21 +12,28 @@ char *_strncat(char *dest, char *src, int n)
 {
 int count1 = 0;
 int count2 = 0;
+int count3, i;
+char *orig1 = src;
 char *orig2 = dest;
 
-while (*dest != '\0')
+while (*dest != 0)
 {
 dest++;
 count1++;
 }
-while (count2 < n && *src != '\0')
+while (*src != 0)
+{
+src++;
+count2++;
+}
+src = orig1;
+count3 = count1 + count2;
+for (i = count3 - count1; i < count1 + n && *src != 0; i++)
 {
 *dest = *src;
 src++;
 dest++;
-count2++;
 }
 dest = orig2;
 return (dest);
 }
-
