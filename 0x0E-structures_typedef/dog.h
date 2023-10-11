4e1565f322fc991e772c5bc4be1ac22b37458785
lailaN123 <lolonj2000@gmail.com>
2023-10-10 21:05:45 -0700

    dog.h file

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - a dog struct
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
