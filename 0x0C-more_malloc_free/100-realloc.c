6297f9d53538e3bb24b45d03fc88665d04a300d5
lailaN123 <lolonj2000@gmail.com>
2023-10-20 22:54:39 -0700

    100-realloc.c file

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -31,18 +31,18 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	if (!p)
 		return (NULL);
 
-	old_p = ptr;
+	o_ptr = ptr;
 
 	if (new_size < old_size)
 	{
 		for (i = 0; i < new_size; i++)
-			p[i] = old_p[i];
+			p[i] = o_ptr[i];
 	}
 
 	if (new_size > old_size)
 	{
 		for (i = 0; i < old_size; i++)
-			p[i] = old_p[i];
+			p[i] = o_ptr[i];
 	}
 
 	free(ptr);
