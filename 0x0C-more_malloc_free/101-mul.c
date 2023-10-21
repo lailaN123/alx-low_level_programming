61bfa70b5076d84ceebd58fb0e4e97956f3f059b
lailaN123 <lolonj2000@gmail.com>
2023-10-20 21:26:14 -0700

    101-mul.c file

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,102 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+#define ERR_MSG "Error"
+
+/**
+ * is_dig - checks if a string contains a non-digit char
+ * @str: string
+ *
+ * Return: 0 if a non-digit is found, 1 otherwise
+ */
+int is_dig(char *str)
+{
+	int i = 0;
+
+	while (str[i])
+	{
+		if (str[i] < '0' || str[i] > '9')
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+/**
+ * _strlen - returns the length of a string
+ * @str: string
+ *
+ * Return: the length of the string
+ */
+int _strlen(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
+/**
+ * error - handles errors for main
+ */
+void error(void)
+{
+	printf("Error\n");
+	exit(98);
+}
+
+/**
+ * main - multiplies two positive numbers
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	char *str1, *str2;
+	int len1, len2, len, i, carry, digit1, digit2, *result, a = 0;
+
+	str1 = argv[1], str2 = argv[2];
+	if (argc != 3 || !is_dig(str1) || !is_dig(str2))
+		errors();
+	len1 = _strlen(str1);
+	len2 = _strlen(str2);
+	len = len1 + len2 + 1;
+	result = malloc(sizeof(int) * len);
+	if (!result)
+		return (1);
+	for (i = 0; i <= len1 + len2; i++)
+		result[i] = 0;
+	for (len1 = len1 - 1; len1 >= 0; len1--)
+	{
+		digit1 = str1[len1] - '0';
+		carry = 0;
+		for (len2 = _strlen(str2) - 1; len2 >= 0; len2--)
+		{
+			digit2 = str2[len2] - '0';
+			carry += result[len1 + len2 + 1] + (digit1 * digit2);
+			result[len1 + len2 + 1] = carry % 10;
+			carry /= 10;
+		}
+		if (carry > 0)
+			result[len1 + len2 + 1] += carry;
+	}
+	for (i = 0; i < len - 1; i++)
+	{
+		if (result[i])
+			a = 1;
+		if (a)
+			_putchar(result[i] + '0');
+	}
+	if (!a)
+		_putchar('0');
+	_putchar('\n');
+	free(result);
+	return (0);
+}
+
